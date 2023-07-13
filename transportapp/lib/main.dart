f36ea284bd69d48dc341a09ff36f3e4d6c9e5513
Basil Njoga <Basilnjoga@gmail.com>
2023-07-14 01:38:34 +0300

    created navigation bar and multiple pages and started initial google maps api additon

--- transportapp/lib/main.dart
+++ transportapp/lib/main.dart
@@ -1,25 +1,131 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Center(
-            child: Text("Live Public Transport Tracking system"),
-            ),
-            backgroundColor: Colors.deepPurple[400],
-        ),
-        body: const Center( 
-          child: Image(image: AssetImage('images/city_bus.jpg'),
-          )
-          
-          ),
-        ),
-      
-      ),
-   
-  
-);
+  runApp(MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Live Transport App',
+        theme: ThemeData(
+            useMaterial3: true,
+            colorScheme: ColorScheme.fromSeed(
+                seedColor: Color.fromRGBO(244, 224, 185, 1.0))),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyAppState extends ChangeNotifier {}
+
+// ...
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = GeneratorPage();
+        break;
+      case 1:
+        page = MapsPage();
+        break;
+      case 2:
+        page = Placeholder();
+        break;
+      case 3:
+        page = Placeholder();
+        break;
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+    }
+    return LayoutBuilder(builder: (context, constraints) {
+      return Scaffold(
+        body: Row(
+          children: [
+            SafeArea(
+              child: NavigationRail(
+                extended: constraints.maxWidth >= 600,
+                destinations: [
+                  NavigationRailDestination(
+                    icon: Icon(Icons.home,
+                        color: Color.fromRGBO(46, 56, 64, 1.0)),
+                    label: Text('Home'),
+                  ),
+                  NavigationRailDestination(
+                    icon: Icon(Icons.place,
+                        color: Color.fromRGBO(46, 56, 64, 1.0)),
+                    label: Text('Maps'),
+                  ),
+                  NavigationRailDestination(
+                      icon: Icon(Icons.update,
+                          color: Color.fromRGBO(46, 56, 64, 1.0)),
+                      label: Text('Activity')),
+                  NavigationRailDestination(
+                      icon: Icon(Icons.account_circle_outlined,
+                          color: Color.fromRGBO(46, 56, 64, 1.0)),
+                      label: Text('Account')),
+                ],
+                selectedIndex: selectedIndex,
+                onDestinationSelected: (value) {
+                  setState(() {
+                    selectedIndex = value;
+                  });
+                },
+              ),
+            ),
+            Expanded(
+              child: Container(
+                color: Theme.of(context).colorScheme.primaryContainer,
+                child: page,
+              ),
+            ),
+          ],
+        ),
+      );
+    });
+  }
+}
+
+class GeneratorPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // var appState = context.watch<MyAppState>();
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image(image: AssetImage('images/city_bus.jpg')),
+          Text("Convinience and Comfort")
+        ],
+      ),
+    );
+  }
+}
+
+class MapsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // var appState = context.watch<MyAppState>();
+    return Scaffold(
+        body: Column(
+      children: [Text('Maps will be displayed here')],
+    ));
+  }
+}
