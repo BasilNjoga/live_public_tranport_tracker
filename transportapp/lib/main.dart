5d7bb50a1957889a7eedb3f3a784bb332940fb42
Basil Njoga <Basilnjoga@gmail.com>
2023-07-27 01:22:14 +0300

    added initial map documents, yet to fix major state error

--- transportapp/lib/main.dart
+++ transportapp/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 
 void main() {
   runApp(MyApp());
@@ -135,21 +137,77 @@ class GeneratorPage extends StatelessWidget {
   }
 }
 
-class MapsPage extends StatelessWidget {
+class MapsPage extends State<MyApp> {
+  late GoogleMapController mapController;
+
+  final LatLng sourceLocation =
+      const LatLng(-1.0909771979474097, 37.01120191308141);
+  final LatLng destination =
+      const LatLng(-1.1248462242034525, 37.00324046871465);
+  final String googleApiKey = "AIzaSyDA5GRgkanc2LJyzcmC62cZ9j82QgVjyKg";
+
+  List<LatLng> polylineCoordinates = [];
+
+  void getPolyPoints() async {
+    PolylinePoints polylinePoints = PolylinePoints();
+
+    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+        googleApiKey,
+        PointLatLng(sourceLocation.latitude, sourceLocation.longitude),
+        PointLatLng(destination.latitude, destination.longitude));
+
+    if (result.points.isNotEmpty) {
+      result.points.forEach(
+        (PointLatLng point) => polylineCoordinates.add(
+          LatLng(point.latitude, point.longitude),
+        ),
+      );
+      setState(() {});
+    }
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
+  void initState() {
+    getPolyPoints();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    // var appState = context.watch<MyAppState>();
-    return Scaffold(
-        body: Column(
-      children: [
-        Text('Choose drop off and pick up point'),
-        Image(height: 400, image: AssetImage('images/map_example.png')),
-        ElevatedButton(
-            onPressed: () {
-              print('button pressed!');
-            },
-            child: Text('Confirm Pickup')),
-      ],
-    ));
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.green[700],
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Maps Sample App'),
+          elevation: 2,
+        ),
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: sourceLocation,
+            zoom: 14.5,
+          ),
+          polylines: {
+            Polyline(
+              polylineId: PolylineId("route"),
+              points: polylineCoordinates,
+            ),
+          },
+          markers: {
+            Marker(
+                markerId: const MarkerId("source"), position: sourceLocation),
+            Marker(
+                markerId: const MarkerId("destination"), position: destination)
+          },
+        ),
+      ),
+    );
   }
 }
