2027e16251978e66d5c2e3ffd290a200abf71646
Basil Njoga <Basilnjoga@gmail.com>
2023-10-17 03:49:12 +0300

    updated

--- backend/publictransport_backend/settings.py
+++ backend/publictransport_backend/settings.py
@@ -42,11 +42,16 @@ INSTALLED_APPS = [
     "passengers.apps.PassengersConfig",
     "vehicles.apps.VehiclesConfig",
     "routes.apps.RoutesConfig",
+    "corsheaders",
 
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
+
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -130,3 +135,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+CORS_ALLOW_ALL_ORIGINS: True
