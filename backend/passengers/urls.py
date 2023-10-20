17fbe5f7efe6f148d6599aec6c2ac2369a0f0431
Basil Njoga <Basilnjoga@gmail.com>
2023-10-20 04:38:18 +0300

    updates

--- backend/passengers/urls.py
+++ backend/passengers/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import PassengerDetailListView, SinglePassengerDetailListView
+from .views import *
 
 
 app_name = "passengers"
