a7581d62a5ea9e05ff7e568300e76d76a0535381
Basil Njoga <Basilnjoga@gmail.com>
2023-10-30 04:29:47 +0300

    fixed geolocation errors and added polylines

--- transportapp/lib/views/map.dart
+++ transportapp/lib/views/map.dart
@@ -1,7 +1,10 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
+import 'dart:developer' as devtools;
 
 class MainMap extends StatefulWidget {
   const MainMap({super.key});
@@ -14,38 +17,132 @@ class _MainMapState extends State<MainMap> {
   final Completer<GoogleMapController> _controller =
       Completer<GoogleMapController>();
 
-  static const CameraPosition _kGooglePlex = CameraPosition(
-      target: LatLng(37.42796133580664, -122.085749655962),
-      zoom: 14.4746,
+
+  static const LatLng sourceLocation = LatLng(-1.071323704835, 37.04846351611011);
+  static const LatLng destination = LatLng(-1.2325829357263685, 36.88116269823942);
+
+  List<LatLng> polylineCoordinates = [];
+  late LatLng currentLocation ;
+
+  void getUserLocation() async {
+     bool serviceEnabled;
+  LocationPermission permission;
+
+  // Test if permissions and location services are enabled.
+  serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!serviceEnabled) {
+    return Future.error('Location services are disabled.');
+  }
+
+  permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      return Future.error('Location permissions are denied');
+    }
+  }
+  
+  if (permission == LocationPermission.deniedForever) { 
+    return Future.error(
+      'Location permissions are permanently denied, we cannot request permissions.');
+  } 
+    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+
+    currentLocation = LatLng(position.latitude, position.longitude);
+
+    setState(() {
+      
+    });
+
+    devtools.log(currentLocation.toString());
+
+  }
+
+  void getPolyPoints() async {
+    PolylinePoints polylinePoints = PolylinePoints();
+
+    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+      'AIzaSyDA5GRgkanc2LJyzcmC62cZ9j82QgVjyKg',
+      PointLatLng(sourceLocation.latitude, sourceLocation.longitude),
+      PointLatLng(destination.latitude, destination.longitude),
       );
 
-  static const CameraPosition _kLake = CameraPosition(
-        bearing: 192.8334901395799,
-        target: LatLng(37.43296265331129, -122.08832357078792),
-        tilt: 59.440717697143555,
-        zoom: 19.151926040649414);
+      if (result.points.isNotEmpty) {
+        result.points.forEach(
+          (PointLatLng point) => 
+              polylineCoordinates.add(LatLng(point.latitude, point.longitude),
+              ),
+            );
+            setState(() {});
+      }
+  }
+
+  @override
+  void initState() {
+    getPolyPoints();
+    getUserLocation();
+    super.initState();
+  }
+
   
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GoogleMap(
-        mapType: MapType.hybrid,
-        initialCameraPosition:_kGooglePlex,
+      body
+      : GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: const CameraPosition(
+          target: sourceLocation,
+          zoom: 14.476),
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
         },
+        polylines: {
+          Polyline(
+            polylineId: const PolylineId("route"),
+            points: polylineCoordinates,
+            width: 10,
+          )
+        },
+        markers: {
+             Marker(
+            markerId: const MarkerId("currentLocation"),
+            position: currentLocation,
+            ),
+          const Marker(
+            markerId: MarkerId("source"),
+            position: sourceLocation,
+            ),
+            const Marker(
+            markerId: MarkerId("destination"),
+            position: destination,
+            ),
+        },
         ),
         
-        floatingActionButton: FloatingActionButton.extended(
-          onPressed: _goToTheLake,
-          label: const Text('To the lake!'),
-          icon: const Icon(Icons.directions_boat),
-          ),
+        floatingActionButton: Center(
+          child: Align(
+          alignment: Alignment.bottomCenter,
+          child: FloatingActionButton.extended(
+            onPressed: _goToDestination,
+            label: const Text('To Destination!'),
+            icon: const Icon(Icons.directions_boat),
+          )
+        ),
+        )
           );
+        
   }
 
-  Future<void> _goToTheLake() async {
+  Future<void> _goToDestination() async {
     final GoogleMapController controller = await _controller.future;
-    await controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+    await controller.animateCamera(CameraUpdate.newCameraPosition(
+      const CameraPosition(
+        target: destination,
+        zoom: 14),
+        ),
+      );
   }
+
+ 
 }
