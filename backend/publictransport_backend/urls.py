e98c922ff4079f1a2ab1a1f22c699b1311202827
Basil Njoga <Basilnjoga@gmail.com>
2023-11-09 03:49:56 +0300

    added register and authtoken endpoint with jwt tokens

--- backend/publictransport_backend/urls.py
+++ backend/publictransport_backend/urls.py
@@ -3,8 +3,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("auth/", include('rest_framework.urls')),
     path("passengers/", include("passengers.urls", namespace="passengers")),
     path("vehicles/",include("vehicles.urls", namespace="vehicles")),
     path("routes/", include("routes.urls", namespace="routes")),
+    path("users/", include("users.urls", namespace="users")),
+    path("auth/", include("authtokens.urls", namespace="authtokens")),
 ]
